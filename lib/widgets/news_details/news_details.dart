c269e04565129481cfae608bd93aa48bb0aaedd6
luijiwa <aratar006@gmail.com>
2023-10-11 22:10:12 +0700

    Добавить отображение из json

--- lib/widgets/news_details/news_details.dart
+++ lib/widgets/news_details/news_details.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:sibcode_test/domain/entity/news_answer.dart';
 
 class NewsDetailsWidget extends StatelessWidget {
-  const NewsDetailsWidget({super.key});
-
+  const NewsDetailsWidget({
+    Key? key,
+    required this.news,
+  }) : super(key: key);
+  final News news;
   @override
   Widget build(BuildContext context) {
+    final String description = news.description ?? '';
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ЭВОЛЮЦИЯ SEO: АДАПТАЦИЯ К ИЗМЕНЕНИЯМ'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            '10.05.2023',
-            style: TextStyle(color: Colors.grey),
-          ),
-          Image.network(
-            'https://webstripe.ru/upload/resize_cache/webp/iblock/adf/l1wj3m728itvinqpxmexcy4npleflcx6/Frame-7.webp',
-          ),
-          const Text(
-            'ЭВОЛЮЦИЯ SEO: АДАПТАЦИЯ К ИЗМЕНЕНИЯМ',
-            style: TextStyle(fontSize: 18),
-          ),
-          const SizedBox(
-            height: 21,
-          ),
-          const Text(
-            'lorem*50',
-          ),
-        ],
+      appBar: AppBar(title: Text(news.name)),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 19,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              news.date,
+              style: const TextStyle(color: Colors.grey),
+            ),
+            Image.network(
+                'https://webstripe.ru/upload/resize_cache/webp/iblock/adf/l1wj3m728itvinqpxmexcy4npleflcx6/Frame-7.webp'),
+            const SizedBox(height: 7),
+            Text(
+              news.name,
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 18),
+            Text(
+              description,
+            ),
+          ],
+        ),
       ),
     );
   }
