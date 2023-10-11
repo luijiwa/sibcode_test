75130f739e73bc4f61345e10a76d878ac28c09f7
luijiwa <aratar006@gmail.com>
2023-10-11 22:05:12 +0700

    Добавить ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sibcode_test/widgets/my_app/my_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
